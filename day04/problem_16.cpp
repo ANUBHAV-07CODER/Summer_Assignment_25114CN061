dac5c9f3aa9ab8e1cf72a1093bda057de3fbf1e3
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-02 11:27:38 +0530

    Add Armstrong number finder in a specified range
    This program prompts the user for a range and prints all Armstrong numbers within that range.

---
+++ day04/problem_16.cpp
@@ -0,0 +1,31 @@
+
+// wap to print armstrong numbers in a range
+#include<bits/stdc++.h>
+using namespace std;
+
+int armstrong_range(int low, int high){
+    for(int i=low; i<=high; i++){
+        int sum =0;
+        int rem;
+        int temp=i;
+        while(temp>0){
+            rem = temp%10;
+            sum = sum + rem*rem*rem;
+            temp = temp/10;
+        }
+        if(sum == i){
+            cout<<i<<" is an armstrong number."<<"\n";
+        }
+    }
+}
+
+int main(){
+    int low, high;
+    cout<<"Enter lower limit: ";
+    cin>>low;
+    cout<<"Enter upper limit: ";
+    cin>>high;
+    armstrong_range(low, high);
+
+    return 0;
+}
