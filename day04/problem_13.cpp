da7fa808259ca575e09cc7459cf3deeaee7b6fb3
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-02 11:24:33 +0530

    Add Fibonacci series generator in C++

---
+++ day04/problem_13.cpp
@@ -0,0 +1,27 @@
+// wap to generate Fibonacci series 
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    if(n==0){
+        return 0;
+    }
+    else if(n==1){
+        return 1;
+    }
+    else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number of terms: ";
+    cin>>n;
+    cout<<"Fibonacci series: ";
+    for(int i=0;i<n;i++){
+        cout<<fib(i)<<" ";
+    }
+    return 0;
+}
+
