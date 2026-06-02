6d121ee9fcf8d3da3fef94ea2ec4ada067bb2fa5
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-02 11:18:37 +0530

    Add program to calculate LCM of two numbers

---
+++ day03/problem_12.cpp
@@ -0,0 +1,18 @@
+
+// wap to find LCM of two numbers
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0){
+        return a;
+    }
+    return gcd(b, a%b);
+}
+int main(){
+    int a, b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"LCM of "<<a<<" and "<<b<<" is "<<(a*b)/gcd(a, b)<<"\n";
+    return 0;
+}
