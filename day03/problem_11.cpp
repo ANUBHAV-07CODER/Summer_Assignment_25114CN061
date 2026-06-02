19dc30ce0e71597766335502f86ba3a4eeff6b85
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-02 11:17:43 +0530

    Add program to find GCD of two numbers

---
+++ day03/problem_11.cpp
@@ -0,0 +1,19 @@
+
+// wap to find GCD of two numbers
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0){
+        return a;
+    }
+    return gcd(b, a%b);
+}
+
+int main(){
+    int a, b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"GCD of "<<a<<" and "<<b<<" is "<<gcd(a, b)<<"\n";
+    return 0;
+}
