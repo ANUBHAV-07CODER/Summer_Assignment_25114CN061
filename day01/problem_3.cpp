505437fbbbb1588e64c702f1fbdbbebd13e341c3
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-02 10:57:47 +0530

    Add program to calculate factorial of a number

---
+++ day01/problem_3.cpp
@@ -0,0 +1,20 @@
+// wap to find factorial of a number
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+    else {
+        return n*fact(n-1);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<"Factorial of "<<n<<" is: "<<fact(n)<<"\n";
+    return 0;
+}
