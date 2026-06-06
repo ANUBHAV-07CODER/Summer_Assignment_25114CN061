3fcff6d2c9d1a169301c312dc1a431a1e9373bf1
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-06 14:28:44 +0530

    Add number triangle printing program
    This program prints a number triangle based on user input for the number of rows.

---
+++ day08/problem_30.cpp
@@ -0,0 +1,17 @@
+
+// wap to print number triangle
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
