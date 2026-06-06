daec183abebea85a3e150b4ef34ea81808285e1e
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-06 14:29:52 +0530

    Add character triangle printing program
    This program prints a character triangle based on the number of rows specified by the user.

---
+++ day08/problem_31.cpp
@@ -0,0 +1,21 @@
+
+// wap to print character triangle
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    char ch;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<char(j+65)<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
