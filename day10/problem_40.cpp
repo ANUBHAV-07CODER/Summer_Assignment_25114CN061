ce6c268ddb0fe82c7abe1cb5724d7d0d6a828308
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-08 13:59:45 +0530

    Add character pyramid printing program
    Implement a program to print a character pyramid based on user input for the number of rows.

---
+++ day10/problem_40.cpp
@@ -0,0 +1,23 @@
+
+// wap to print character pyramid
+ #include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter number of rows:";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(char ch='A';ch<='A'+i;ch++){
+            cout<<ch;
+        }
+        for(char ch='A'+i-1;ch>='A';ch--){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+    return 0 ;
+}
