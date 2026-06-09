e7f3a05b7bf8472d1e0cf6f905e7d26734567534
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-09 14:00:33 +0530

    Implement function to sum two numbers

---
+++ day11/problem_41.cpp
@@ -0,0 +1,18 @@
+
+// wap to write function to find sum of two number
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum_of_two_num(int a, int b){
+    return a+b;
+}
+int main(){
+    int x;
+    cout<<"Enter x:";
+    cin>>x;
+    int y;
+    cout<<"Enter y:";
+    cin>>y;
+    cout<<"the sum of two number x and y is: "<<sum_of_two_num(x,y);
+    return 0;
+}
