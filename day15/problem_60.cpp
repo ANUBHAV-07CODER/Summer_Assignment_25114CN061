2b0aeaac88342886d2b74d2c1ad0c8b948aee68f
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-13 12:37:38 +0530

    Add function to move zeroes to the end of array
    Implemented a function to move all zeroes in an array to the end while maintaining the order of non-zero elements.

---
+++ day15/problem_60.cpp
@@ -0,0 +1,28 @@
+// wap to move zeroes to end
+#include<bits/stdc++.h>
+using namespace std;
+
+int movezeroes(int arr[],int n){
+    int j=0;
+    for(int i=0;i<n;i++){
+     if(arr[i] != 0){
+        swap(arr[i],arr[j]);
+        j++;
+        }
+    }
+}
+int main(){
+     int n;
+    cout<<"Enter size:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter element:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    movezeroes(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
