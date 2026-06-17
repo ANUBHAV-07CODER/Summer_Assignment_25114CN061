da612164932603e1e66715db5d5d9092179b667e
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-17 13:47:44 +0530

    Add matrix addition program in C++

---
+++ day19/problem_73.cpp
@@ -0,0 +1,33 @@
+// wap to add matrices
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   int n,m;
+   int a[3][3],b[3][3];
+   cout<< "enter row:";
+   cin>>n;
+   cout<< "enter column:";
+   cin>>m;
+   for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        cout<<"enter first matrix element:"<<endl;
+        cin>>a[i][j];
+    }
+   }
+    for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        cout<<"enter second matrix element:"<<endl;
+        cin>>b[i][j];
+    }
+   }
+    cout<<"the sum of first and second matrix is :\n";
+    for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+       cout<<a[i][j] + b[i][j]<<" ";
+    }
+    cout<<endl;
+   }
+   return 0;
+}
