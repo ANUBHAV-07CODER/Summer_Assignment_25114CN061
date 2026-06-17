e3dbec9448695576c4cec893387e7571fa2679c0
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-17 10:24:44 +0530

    Fix function parameter type for descending order sort

--- day18/problem_72.cpp
+++ day18/problem_72.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int descendingorder(int arr,int n){
+int descendingorder(int arr[],int n){
     int temp;
     for(int i=0;i<n-1;i++){
         for(int j = 0;j<n-i-1;j++){
