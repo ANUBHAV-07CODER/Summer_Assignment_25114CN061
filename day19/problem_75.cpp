b0843f48b226217d471d73132fa178c23fc30974
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-18 15:22:03 +0530

    Fix loop indices for transposing matrix

--- day19/problem_75.cpp
+++ day19/problem_75.cpp
@@ -21,8 +21,8 @@ int main(){
     }
    }
    cout<<"transpose matrix is: \n";
-    for(int i=0;i<m;i++){
-    for(int j=0;j<n;j++){
+    for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
         cout<<t[i][j]<<" ";
     }
     cout<< endl;
