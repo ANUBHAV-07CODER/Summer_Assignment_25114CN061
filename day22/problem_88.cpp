8155bf6d407e1dc8a662dbee1c6aab50352d4279
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-20 15:08:49 +0530

    Add program to remove spaces from a string

---
+++ day22/problem_88.cpp
@@ -0,0 +1,19 @@
+// wap to remove spaces from string
+ #include<bits/stdc++.h>
+using namespace std;
+
+ int main(){
+    char str[100];
+    cout<<"Enter a sentence:";
+    cin.getline(str,100);
+    int j=0;
+   for(int i=0;str[i] != '\0';i++){
+        if(str[i] != ' '){
+         str[j] = str[i];
+         j++;
+        }
+    }
+    str[j] = '\0';
+    cout<< str;
+    return 0;
+}
