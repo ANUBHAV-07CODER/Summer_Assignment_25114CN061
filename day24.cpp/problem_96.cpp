d746e79d449c5b5da9070bb7f40e0d58ee6325f2
ANUBHAV-07CODER <anubhavsingh8077a@gmail.com>
2026-06-24 14:26:05 +0530

    Add program to remove duplicate characters
    Implement a program to remove duplicate characters from a string.

---
+++ day24.cpp/problem_96.cpp
@@ -0,0 +1,23 @@
+// wap to remove duplicate character
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    cout<<"Enter string:";
+    cin>>str;
+
+    for(int i =0;i<str.length();i++){
+        bool duplicate = false;
+        for(int j=0;j<i;j++){
+            if(str[i]==str[j]){
+                duplicate = true;
+                break;
+            }
+        }
+        if(!duplicate){
+            cout<<str[i];
+        }
+    }
+    return 0;
+}
